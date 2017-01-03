f36e6ce343e9cb47774851628a2de6f263951b7a
Wind <382690308@qq.com>
2017-01-04 00:21:08 +0800

    react first

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform:ios,'8.0'
+target:RWReactivePlayground do
+use_frameworks!
+pod 'ReactiveCocoa', '5.0.0-rc.1'
+end
---
+++ Pods/Target Support Files/Pods-RWReactivePlayground/Pods-RWReactivePlayground-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_RWReactivePlaygroundVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RWReactivePlaygroundVersionString[];
+
